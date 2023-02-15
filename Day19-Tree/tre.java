471f2d2915220982d490a2a4e853bc1e7c3dc6ff
rithiksanjeev <46381099+RITHIKSANJEEV-KUMAR@users.noreply.github.com>
2023-02-15 20:54:41 +0530

    tree

--- Day19-Tree/tre.java
+++ Day19-Tree/tre.java
@@ -22,7 +22,8 @@ public class tre {
     public static void main(String[] args) {
         
     }
-    
+    // eg:code in https://leetcode.com/problems/binary-tree-level-order-traversal/
+    //uler below fun is the base
     public void fun(TreeNode root)
     {
         //base case
@@ -33,5 +34,34 @@ public class tre {
         System.out.println("visitng "+root.val+" Second Time");
         fun(root.right);
         System.out.println("visitng "+root.val+" Third Time");
+    }
+
+    public void preorder(TreeNode root)
+    {
+            //base case
+            if(root == null)
+            return;
+            System.out.println("visitng "+root.val+" Pre area"); // pre area
+            preorder(root.left);
+            preorder(root.right);
+           
+    }
+    public void inorder(TreeNode root)
+    {
+            //base case
+            if(root == null)
+            return;
+            inorder(root.left);
+            System.out.println("visitng "+root.val+" in area"); // pre area
+            inorder(root.right);
+    }
+    public void postorder(TreeNode root)
+    {
+            //base case
+            if(root == null)
+            return;
+            postorder(root.left);
+            postorder(root.right);
+            System.out.println("visitng "+root.val+" post area"); // pre area
     }
 }
